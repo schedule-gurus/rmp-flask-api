464703d310586cf88f2aa1afe8fa4ddabce0a896
Nirav Adunuthula <adunuthulan@gmail.com>
2020-11-05 11:56:19 -0800

    initial commit:

---
+++ app.py
@@ -0,0 +1,24 @@
+#app.py#Import necessary packages
+from flask import Flask
+from flask_restful import Resource, reqparse, Api #Instantiate a flask object 
+app = Flask(__name__)
+#Instantiate Api object
+api = Api(app)
+
+#import API from RMPClass
+from RMPClass import RateMyProfAPI
+
+class RMP(Resource):
+    def get(self, name):    
+        rmp = RateMyProfAPI(teacher = name)  
+        rmp.retrieve_rmp_info()
+        if rmp.get_rmp_info() == "Info currently not available":            
+            return {'Message': 'Professor/Teacher not found'}
+        return rmp.get_rmp_info()  
+
+api.add_resource(RMP, '/<string:name>')
+
+if __name__=='__main__':        
+    #Run the applications
+    app.run() 
+    
